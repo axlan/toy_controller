1d8b3995a17ae453fcab1faedca1423948f6b60d
Jonathan Diamond <feros32@gmail.com>
2023-04-12 15:27:55 -0700

    Add simple WLED control demo.

---
+++ python/wled_demo.py
@@ -0,0 +1,88 @@
+import paho.mqtt.client as mqtt
+import json
+import requests
+import dataclasses
+
+@dataclasses.dataclass
+class State:
+    selected = 0
+    color = [0, 0, 0]
+    sent = True
+    last_buttons = []
+
+state = State()
+
+# Define MQTT callbacks
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("controller/buttons_pressed")
+
+def on_message(client, userdata, msg):
+    json_str = msg.payload.decode('ascii')
+    print(msg.topic+" "+json_str)
+    new_buttons = json.loads(json_str)
+    pressed = [b for b in new_buttons if b not in state.last_buttons]
+    state.last_buttons = new_buttons
+
+    if len(pressed) == 0:
+        return
+
+    state.sent = False
+    if 'LEFT' in pressed:
+        state.selected = 2
+    elif 'RIGHT' in pressed:
+        state.selected = 0
+    elif 'UP' in pressed:
+        state.selected = 1
+    elif 'DOWN' in pressed:
+        state.selected = 3
+
+    if 'A' in pressed:
+        state.color = [0, 255, 0]
+    elif 'B' in pressed:
+        state.color = [0, 0, 255]
+    elif 'C' in pressed:
+        state.color = [255, 0, 0]
+    elif 'D' in pressed:
+        state.color = [255, 255, 0]
+
+# Create an MQTT client
+client = mqtt.Client()
+
+# Set MQTT callbacks
+client.on_connect = on_connect
+client.on_message = on_message
+
+# Connect to the MQTT broker
+client.connect("192.168.1.110", 1883, 60)
+
+wled_url = "http://192.168.1.123/json/state"
+
+BUTTON_LOCATIONS = {
+    'LEFT': (135, 385),
+    'RIGHT': (250, 362),
+    'UP': (185, 315),
+    'DOWN': (208, 427),
+    'A': (470, 197),
+    'B': (570, 145),
+    'C': (628, 235),
+    'D': (535, 287),
+}
+
+# resp = requests.get(wled_url)
+# cur_state = json.dumps(resp.json())
+
+while True:
+    client.loop(timeout=1.0, max_packets=1)
+
+    if not state.sent:
+        state.sent = True
+
+        resp = requests.post(wled_url, json={
+            'seg': [{
+                'id': state.selected,
+                'col': [state.color, [0, 0, 0], [0, 0, 0]]
+            }]
+        })
+        print(resp.json())
